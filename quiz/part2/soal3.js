94c0a6267abfc68fee6a52caf4c544943b9f1d9b
LewzzJrs <nainggolanlewi3@gmail.com>
2025-08-20 20:16:37 +0700

    Submit Tugas Kedua (QuizPart2)

---
+++ quiz/part2/soal3.js
@@ -0,0 +1,17 @@
+//Soal 3. Breaking Sentence (Again) using Substring
+//js
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = 'wow JavaScript is so cool';
+let exampleFirstWord = word3.substring(0,3);
+let exampleSecondWord = word3.substring(4,14);
+let exampleThirdWord = word3.substring(14,17);
+let exampleFourthWord= word3.substring(18,20);
+let exampleFifthWord = word3.substring(21,25);
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + exampleSecondWord);
+console.log('Third Word: ' + exampleThirdWord);
+console.log('Fourth Word: ' + exampleFourthWord);
+console.log('Fifth Word: ' + exampleFifthWord); 
